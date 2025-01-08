40b0edea657f0774d2d65a831c0efd14e5ba99ca
deolinda-gabriela <deolindagabriela@gmail.com>
2025-01-07 23:49:23 -0300

    Add files via upload
    init

---
+++ script.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('controlador',{
+    init: function() {
+      this.toggle = false;
+      this.vid = document.querySelector("#video");
+      this.vid.pause();
+    },
+    tick: function() {
+      if (this.el.object3D.visible == true) {
+        if(!this.toggle) {
+          this.toggle =true;
+          this.vid.play();
+        }
+      } else {
+        this.toggle = false;
+        this.vid.pause();
+      }
+    }
+});
